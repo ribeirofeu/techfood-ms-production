9f36081bbcb1b275f31475caf631b989cfd97131
Alexandre Zelante <alexandre.zelante@ibm.com>
2024-01-14 15:21:34 -0300

    initial commit with spring and mongo

---
+++ src/main/java/com/fiap/techchallenge4/fiaptechchallenge4/FiapTechchallenge4Application.java
@@ -0,0 +1,13 @@
+package com.fiap.techchallenge4.fiaptechchallenge4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapTechchallenge4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiapTechchallenge4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/techchallenge4/fiaptechchallenge4/FiapTechchallenge4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.techchallenge4.fiaptechchallenge4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapTechchallenge4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
