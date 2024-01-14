2e6e74680581cd71c0cbd09144f5d24c4d8ba1dd
Alexandre Zelante <alexandre.zelante@ibm.com>
2024-01-14 16:00:33 -0300

    gitignore and properties

--- src/main/java/com/fiap/techchallenge4/fiaptechchallenge4/FiapTechchallenge4Application.java
+++ src/main/java/com/fiap/techchallenge4/fiaptechchallenge4/FiapTechchallenge4Application.java
@@ -2,8 +2,10 @@ package com.fiap.techchallenge4.fiaptechchallenge4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class FiapTechchallenge4Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/fiap/techchallenge4/fiaptechchallenge4/model/Production.java
+++ src/main/java/com/fiap/techchallenge4/fiaptechchallenge4/model/Production.java
@@ -1,2 +1,18 @@
-package com.fiap.techchallenge4.fiaptechchallenge4.model;public class Production {
+package com.fiap.techchallenge4.fiaptechchallenge4.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDateTime;
+
+@Document
+public class Production {
+
+    @Id
+    private String id;
+    private String orderId;
+    private LocalDateTime receivedDate;
+    private String status;
+
 }
