6cb14bb34aa8b2bd5d4c2038340beb2b616f333a
Alexandre Zelante <alexandre.zelante@outlook.com>
2024-01-16 20:09:40 -0300

    docs: add swagger

--- src/main/java/com/fiap/techchallenge4/fiaptechchallenge4/controller/ProductionController.java
+++ src/main/java/com/fiap/techchallenge4/fiaptechchallenge4/controller/ProductionController.java
@@ -4,22 +4,27 @@ import com.fiap.techchallenge4.fiaptechchallenge4.domain.Production;
 import com.fiap.techchallenge4.fiaptechchallenge4.dtos.request.AddProductionDTO;
 import com.fiap.techchallenge4.fiaptechchallenge4.dtos.request.SetProductionStatusDTO;
 import com.fiap.techchallenge4.fiaptechchallenge4.service.ProductionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/production")
+@Tag(name = "Produção")
 public class ProductionController {
 
     private ProductionService productionService;
 
     @PostMapping
+    @Operation(summary = "Adiciona uma nova produção")
     public Production addProduction(@RequestBody AddProductionDTO addProductionDTO) {
         return productionService.addProduction(addProductionDTO.orderId());
     }
 
     @PutMapping("/{orderId}")
+    @Operation(summary = "Atualiza o status de uma produção")
     public Production setProductionStatus(@PathVariable String orderId, @RequestBody SetProductionStatusDTO setProductionStatusDTO) {
         return productionService.setStatus(orderId, setProductionStatusDTO.status());
     }
