615aac5d6a538bed417fc5724117e19ad83d40eb
Lucas Soares <lucas.soares.araujo@hotmail.com>
2024-01-29 19:29:58 -0300

    ajusta configuração de testes

--- src/test/java/com/fiap/techchallenge4/fiaptechchallenge4/application/usecases/ProductionUseCasesImplTest.java
+++ src/test/java/com/fiap/techchallenge4/fiaptechchallenge4/application/usecases/ProductionUseCasesImplTest.java
@@ -21,7 +21,6 @@ import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 
 public class ProductionUseCasesImplTest {
